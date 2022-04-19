fb72bf9ff384ed010b32e6c5c9b4e27fbfeed25a
Barsik_Kato <noran-kotov@yandex.ru>
2022-04-19 09:24:28 +0300

    Добавьте файлы проекта.

---
+++ KotovExam2022/Classes/BaseConnect.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KotovExam2022
+{
+    internal class BaseConnect
+    {
+        public static Entities baseModel;
+    }
+}
